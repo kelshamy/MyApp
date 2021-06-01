a7e78370a7bba6a2ac22bc59c468f93bb3e75444
kelshamy <k.elshamy@live.com>
2021-05-31 23:14:01 -0400

    First commit

---
+++ android/app/src/main/kotlin/com/kelshamy/new_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kelshamy.new_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
