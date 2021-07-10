52f7d735bf8fc6af8993af442799f16bc77e21d6
kelshamy <k.elshamy@live.com>
2021-07-10 19:46:53 -0400

    appbar and scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp {}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This Is My Default Text '),
+      ),
+    );
+  }
+}
